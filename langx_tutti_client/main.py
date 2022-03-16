bd2e3179851f89711f81c49681f7aa079f9faa38
ssmsaito <saito@iflab.tokyo>
2022-03-16 19:05:48 +0900

    added docs

--- langx_tutti_client/main.py
+++ langx_tutti_client/main.py
@@ -17,6 +17,23 @@ class LangXTuttiClientEnvironmentError(Exception):
         self.err = err
 
 class LangXTuttiClient:
+    '''LangXクライアントの各種機能を使うための窓口（Facade）となるクラスです。
+
+    このオブジェクトがメソッドとして直接管理する操作は以下の２つのみです。
+    
+    - :meth:`publish_scatt_tasks_to_market` ... アノテーション対象データをタスクにし、Tutti.marketへタスク発注する
+    - :meth:`watch_responses_for_scatt_tasks` ... タスクへの回答の取得およびプッシュ通知を受ける
+
+    これに加えて、Scatt、Tutti.works、Tutti.marketへの個別の操作が必要となる場合は、それぞれ :attr:`scatt` 、 :attr:`tutti` 、 :attr:`market` メンバーから各種メソッドにアクセス可能です。
+
+    Attributes:
+        scatt (ScattController): Scattリソースアクセス用オブジェクト
+        tutti (`tutti_client.TuttiClient (外部リンク)`_): Tutti.worksリソースアクセス用オブジェクト
+        market (TuttiMarketController): Tutti.marketリソースアクセス用オブジェクト
+
+    .. _tutti_client.TuttiClient (外部リンク):
+        https://iflb.github.io/tutti-client-python/references/facade.html
+    '''
     def __init__(self):
         super()
         self.scatt = ScattController(Duct())
@@ -29,6 +46,16 @@ class LangXTuttiClient:
             raise LangXTuttiClientEnvironmentError(e) from e
 
     async def open(self, scatt_host: Optional[str] = None, works_host: Optional[str] = None, market_host: Optional[str] = None) -> None:
+        '''Scatt、Tutti.works、Tutti.marketのサーバーへ接続します。
+        
+        LangXTuttiClientのメソッドを呼び出す前に必須の処理です。
+        内部的には、:meth:`open_scatt`、:meth:`open_tutti`、:meth:`open_market` を一度に実行します。
+
+        Args:
+            scatt_host: Scattサーバーのホスト名
+            works_host: Tutti.worksサーバーのホスト名
+            market_host: Tutti.marketサーバーのホスト名
+        '''
         tasks = []
         if scatt_host: tasks.append(self.open_scatt(scatt_host))
         if works_host: tasks.append(self.open_works(works_host))
@@ -37,6 +64,13 @@ class LangXTuttiClient:
         await asyncio.gather(*tasks)
 
     async def open_scatt(self, host: str) -> None:
+        '''Scattサーバーへ接続します。
+        
+        LangXTuttiClientのメソッドを呼び出す前に必須の処理です。
+
+        Args:
+            host: Scattサーバーのホスト名
+        '''
         err = []
         async def on_error(event):
             err.append(event)
@@ -49,6 +83,13 @@ class LangXTuttiClient:
             raise LangXTuttiClientConnectionError('scatt', err[0])
 
     async def open_works(self, host: str) -> None:
+        '''Tutti.worksサーバーへ接続します。
+        
+        LangXTuttiClientのメソッドを呼び出す前に必須の処理です。
+
+        Args:
+            host: Tutti.worksサーバーのホスト名
+        '''
         err = []
         async def on_error(event):
             err.append(event)
@@ -61,6 +102,13 @@ class LangXTuttiClient:
             raise LangXTuttiClientConnectionError('Tutti.works', err[0])
 
     async def open_market(self, host: str) -> None:
+        '''Tutti.marketサーバーへ接続します。
+        
+        LangXTuttiClientのメソッドを呼び出す前に必須の処理です。
+
+        Args:
+            host: Tutti.marketサーバーのホスト名
+        '''
         err = []
         async def on_error(event):
             err.append(event)
@@ -73,37 +121,85 @@ class LangXTuttiClient:
             raise LangXTuttiClientConnectionError('Tutti.market', err[0])
 
     def close(self) -> None:
+        '''Scatt、Tutti.works、Tutti.marketのサーバー接続を切断します。
+        '''
         self.close_scatt()
         self.close_works()
         self.close_market()
 
     def close_scatt(self) -> None:
+        '''Scattサーバーへの接続を切断します。
+        '''
         self.scatt.close()
 
     def close_works(self) -> None:
+        '''Tutti.worksサーバーへの接続を切断します。
+        '''
         self.tutti.close()
 
     def close_market(self) -> None:
+        '''Tutti.marketサーバーへの接続を切断します。
+        '''
         self.market.close()
 
-    async def sign_in(self, works_params: Optional[dict], market_params: Optional[dict]) -> None:
+    async def sign_in(self, works_params: dict, market_params: dict) -> None:
+        '''Tutti.worksおよびTutti.marketへサインインします。
+
+        内部的には、:meth:`sign_in_works` および:meth:`sign_in_market` を順に実行します。
+
+        Args:
+            works_params: :meth:`sign_in_works` へ渡すキーワード引数をメンバーとして持つdict。
+            market_params: :meth:`sign_in_market` へ渡すキーワード引数をメンバーとして持つdict。
+        '''
         await self.sign_in_works(**works_params)
         await self.sign_in_market(**market_params)
 
     async def sign_in_market(self, user_id: str, password: str, access_token_lifetime: int = 60*60*24*7*1000) -> None:
+        '''Tutti.marketへサインインします。
+
+        Args:
+            user_id: ユーザーID
+            password: パスワード
+            access_token_lifetime: アクセストークンの有効期限（ミリ秒）
+        '''
         await self.market.sign_in(user_id, password, access_token_lifetime)
 
-    async def sign_in_works(self, user_name: str = None, password_hash: str = None, access_token: str = None, called = True, **kwargs) -> None:
-        await self.tutti.resource.sign_in(user_name, password_hash, access_token, called, **kwargs)
+    async def sign_in_works(self, user_name: Optional[str] = None, password_hash: Optional[str] = None, access_token: Optional[str] = None, **kwargs) -> None:
+        '''Tutti.worksへサインインします。
+
+        引数の渡し方は複数通り存在します。
+
+        1. ``access_token`` のみを指定する
+        2. ``user_name`` と ``password_hash`` を指定する
+        3. ``user_name`` と ``password`` （ ``kwargs`` として受け取られる）を指定する
+
+        ※ 1→2→3の順に優先されます。
+        :meth:`sign_in` においては3が適用されています。
+
+        Args:
+            user_name: ユーザー名
+            password_hash: パスワードのハッシュ値（MD5）
+            access_token: アクセストークン
+            kwargs: ``password`` が指定可能です。
+        '''
+        await self.tutti.resource.sign_in(user_name, password_hash, access_token, **kwargs)
 
     async def sign_out(self) -> None:
+        '''Tutti.market、Tutti.worksからサインアウトします。
+
+        内部的には :meth:`sign_out_market` 、 :meth:`sign_out_works` が順に呼ばれます。
+        '''
         await self.sign_out_market()
         await self.sign_out_works()
 
     async def sign_out_market(self) -> None:
+        '''Tutti.marketからサインアウトします。
+        '''
         await self.market.sign_out()
 
     async def sign_out_works(self) -> None:
+        '''Tutti.worksからサインアウトします。
+        '''
         await self.tutti.resource.sign_out()
 
 
@@ -116,6 +212,22 @@ class LangXTuttiClient:
         elan_tsv_file_path: Optional[str] = None,
         overwrite_files: bool = False,
     ) -> Tuple[str, str]:
+        '''アノテーション対象データをScattサーバーへ登録し、タスクをTutti.marketのJobとして発行します。
+
+        Args:
+            automation_parameter_set_id: Tutti.worksにおいて発行されるAutomation Parameter Set ID (Automation ID)。
+            student_id: インタビュイーの生徒ID。任意の数列を文字列で指定可能（想定は先頭0埋め7桁）です。
+            video_id: 生徒IDに対するビデオID。任意の数列を文字列で指定可能（想定は先頭0埋め5桁）です。
+            video_file_path: ビデオファイル（mp4）のローカルパス。
+            elan_tsv_file_path: 音声認識結果や事前ラベル情報等を持ったファイル（TSV）のローカルパス。
+            overwrite_files: ``True`` の時、 ``student_id`` と ``video_id`` の組がすでに存在していてもファイルを上書きします。
+
+        Returns:
+            tuple: 生成されたリソースのIDを返します。0番目はTutti.worksのNanotask Group ID、1番目はTutti.marketのJob IDです。
+
+        Raises:
+            ScattFileStorageError: ``student_id`` と ``video_id``の組がすでにScattサーバー上に登録済みで、``video_file_path`` が引数に指定されており、かつ ``overwrite_files=True`` の時。
+        '''
 
         if video_file_path:
             await self.scatt.prepare_and_upload_files(student_id, video_id, video_file_path, elan_tsv_file_path, overwrite_files)
@@ -211,6 +323,28 @@ class LangXTuttiClient:
         handler: Callable[[dict], None],
         last_watch_id: str = '+'
     ) -> None:
+        '''発注したScatt UIのJobにおいて、Tutti.worksに保存された回答をプッシュ通知で受け取ります。
+
+        また、このメソッドの実行時、 :attr:`last_watch_id` よりも後に収集された回答をサーバーから順番に返します。
+        これは例えば、前回 :meth:`watch_responses_for_scatt_tasks` を実行していたプロセスが終了してから、
+        次回再びプロセスを呼ぶまでの間に既に発行済みのJobにおいて新たな回答があった場合に、それらの回答を次回実行時に
+        まとめて取得するという目的で有効に用いることができます。
+
+        Args:
+            automation_parameter_set_id: Tutti.worksで発行されたAutomation Parameter Set ID
+            handler: 回答を受信するたびに実行される（asyncコルーチン）関数。引数を１つとり、回答情報を受け取ります。
+                この引数はdict型で、 ``last_watch_id`` と ``data`` の２つのキーを持ちます。
+            last_watch_id: 過去に受け取った回答のWatch IDを指定すると、指定値よりも後のタイムスタンプに相当する
+                回答を全て返します。Watch IDの形式は ``<ミリ秒タイムスタンプ>-<0始まり通し番号>`` で、
+                Tutti.worksの内部データベースであるRedisの `Stream ID`_ に準拠します。したがって、この値に
+                ハイフン以降を除いた ``<ミリ秒タイムスタンプ>`` を渡した場合はその時間以降の全ての回答が返され、
+                またデフォルト値の ``+`` は正の方向の無限大値を指すため、メソッド実行時に過去の回答は返されません。
+                ``0`` を指定することで 履歴に存在する全ての回答を受け取ることが可能ですが、収集済み回答数が多く
+                なっている場合は受信データが膨大になる可能性があるため、非推奨です。
+        
+        .. _Stream ID:
+            https://redis.io/topics/streams-intro
+        '''
         self.tutti.resource.on('watch_responses_for_automation_parameter_set', handler)
         await self.tutti.resource.watch_responses_for_automation_parameter_set.send(
             automation_parameter_set_id = automation_parameter_set_id,
--- langx_tutti_client/market_controller.py
+++ langx_tutti_client/market_controller.py
@@ -2,13 +2,42 @@ from typing import Optional
 import hashlib
 
 class TuttiMarketController:
+    '''Tutti.marketに関連する操作を行うオブジェクトです。
+
+    現状、必要最低限のメソッド群のみを定義しています。JavaScriptにおいて既に `こちら`_ に定義された
+    操作のうち、このクラスに実装が必要なものは別途問い合わせてください。
+
+    .. _こちら:
+        https://github.com/iflb/tutti-market/blob/a4b6b9054183f761a1692ff9633a84e80d93ea3c/frontend/src/scripts/ducts.js#L142
+    '''
     def __init__(self, duct):
         self._duct = duct
 
     async def open(self, wsd_url: str):
+        '''Tutti.marketサーバーへ接続します。
+
+        Args:
+            wsd_url: DUCTSサーバーへ接続するエンドポイント
+        '''
         await self._duct.open(wsd_url)
 
-    async def register_job(self, job_class_id: str, job_parameter: Optional[dict] = True, description: Optional[str] = None, num_job_assignments_max: Optional[int] = None, priority_score: Optional[int] = None):
+    async def register_job(
+        self,
+        job_class_id: str,
+        job_parameter: Optional[dict] = None,
+        description: Optional[str] = None,
+        num_job_assignments_max: Optional[int] = None,
+        priority_score: Optional[int] = None
+    ) -> dict:
+        '''ジョブを発注します。
+
+        Args:
+            job_class_id: ジョブクラスID
+            job_parameter: 発注するジョブへ与えるパラメータ群
+            description: ジョブの説明文（リクエスタから見えるメモ用）
+            num_job_assignments_max: 収集する回答数上限
+            priority_score: 優先度。値が小さいほど優先度が高く、優先的にワーカーへ割り当てられます。
+        '''
         data = await self._duct.call(self._duct.EVENT['REGISTER_JOB'], {
                 'access_token': self.access_token,
                 'job_class_id': job_class_id,
@@ -20,6 +49,8 @@ class TuttiMarketController:
         return data
 
     async def sign_in(self, user_id: str, password: str, access_token_lifetime: int):
+        '''Tutti.marketにサインインします。
+        '''
         data = await self._duct.call(self._duct.EVENT['SIGN_IN'], {
             'user_id': user_id,
             'password_hash': hashlib.sha512(password.encode('ascii')).digest(),
@@ -28,6 +59,8 @@ class TuttiMarketController:
         self.access_token = data['body']['access_token']
 
     async def sign_out(self):
+        '''Tutti.marketからサインアウトします。
+        '''
         data = await self._duct.call(self._duct.EVENT['SIGN_OUT'], {
             'access_token': self.access_token
         })
