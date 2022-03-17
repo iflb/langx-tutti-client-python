0b648cad74c3ced75974e1977b64cf5f24bb38ea
Kenshiro Ueda <kenshiro.ueda@gmail.com>
2022-03-17 12:07:25 +0900

    changed path

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, '/Users/susumu/programming/python/langx-tutti-client-python')
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 
 # -- Project information -----------------------------------------------------
